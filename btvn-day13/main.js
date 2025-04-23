2f87f4f3deabeacc5524177bc89e8bf6835f44ac
vietanh1603 <anhvhf8@fullstack.edu.vn>
2025-04-24 06:55:30 +0700

    bai tập ve nha buoi 13

---
+++ btvn-day13/main.js
@@ -0,0 +1,154 @@
+// bai 1 tim min cua array
+
+/*
+
+
+          ┌───────────────────┐
+          │                   │                    ┌───────────────────┐
+          │ let min = arr[0]  │                    │                   │
+          │   minIndex = 0    │                    │ let max = arr[0]  │
+          └────────┬──────────┘                    │   maxIndex = 0    │
+                   │                               └────────┬──────────┘
+                   │                                        │
+                   ▼                                        │
+    ┌───────────────────────────────────┐                   ▼
+    │   let i = 1; i < arr.length, i++  │    ┌───────────────────────────────────┐
+    └──────────────┬────────────────────┘    │   let i = 1; i < arr.length, i++  │
+                   │                         └──────────────┬────────────────────┘
+                   │                                        │
+                   ▼                                        │
+            ┌──────────────────┐                            ▼
+            │  arr[i] , min    │                     ┌──────────────────┐
+            │   min = arr[i]   │                     │  arr[i] , min    │
+            │  minIndex = i    │                     │   max = arr[i]   │
+            └─────┬────────────┘                     │   maxIndex = i   │
+                  │                                  └─────┬────────────┘
+                  ▼                                        │
+         ┌───────────────────────┐                         ▼
+         │   gia tri nho nhat cua│                ┌───────────────────────┐
+         │    day so = min       │                │   gia tri nho nhat cua│
+         │                       │                │    day so = max       │
+         │   vi tri = minIndex   │                │                       │
+         └───────────────────────┘                │    vi tri = mixIndex  │
+                                                  └───────────────────────┘
+
+
+ */
+let arr = [3,65,12,64,2] ;
+let min = arr[0] ;
+let minIndex =0 ;
+let max = arr[0] ;
+let maxIndex = 0 ;
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+        min = arr[i] ;
+        minIndex = i ;
+    }
+}
+console.log('min cua day so la ', min)
+console.log('vi tri gia tri min cua day so la', minIndex)
+
+
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+        max = arr[i] ;
+        maxIndex = i ;
+    }
+}
+console.log('max cua day so la ', max)
+console.log( 'vi tri gia tri max cua day so la', maxIndex)
+
+
+// bai 2
+/*
+
+
+              ┌──────────┐
+              │          │
+              │ let arr  │
+              └───┬──────┘
+                  │
+                  │
+         ┌────────▼─────────────┐
+         │ i=0, i < arr.length  │◄────┐
+         └────────┬─────────────┘     │
+                  │                   │
+                  │                   │
+         ┌────────▼─────────┐         │
+         │ trungLap= false  │         │
+         └────────┬─────────┘         │
+                  │                   │
+                  ▼                   │
+         ┌───────────────────────┐    │
+         │ j=0, j < arr.length   │    │
+         └────────┬──────────────┘    │
+                  │                   │
+                  │                   │
+                  ▼                   │
+          ┌────────────────┐ false    │
+          │arr[i]=== arr[j]├──────────┘
+          └───────┬────────┘
+                  │  true
+                  │  break
+                  ▼
+          ┌─────────────────┐
+          │ if (!trungLap)  │
+          │                 │
+          │ arr.push(arr[i] │
+          └──────┬──────────┘
+                 │
+                 ▼
+         ┌────────────────────┐
+         │  console.log(arr)  │
+         └────────────────────┘
+
+ */
+let bai2 = [1, 2, 3, 2, 4, 3, 5];
+let result = [];
+
+for (let i = 0; i < bai2.length; i++) {
+    let trungLap = false;
+
+    for (let j = 0; j < result.length; j++) {
+        if (bai2[i] === result[j]) {
+            trungLap = true;
+            break;
+        }
+    }
+
+    if (!trungLap) {
+        result.push(bai2[i]);
+    }
+}
+
+console.log(result); // [1, 2, 3, 4, 5]
+
+// bài 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
