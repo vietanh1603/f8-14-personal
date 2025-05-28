8a2990b982a9d2f5e629a263233787fe5b35cd01
vietanh <anhvhf8@fullstack.edu.vn>
2025-05-28 23:26:51 +0700

    bai tap ve nha day 25

---
+++ btvn-day25/pracrtice/maxOrder.js
@@ -0,0 +1,9 @@
+export const findHighestValueOrder = (orders) => {
+    if (orders.length === 0) return null;
+
+    return orders.reduce((highest, order) => {
+        const currentTotal = order.calculateTotal();
+        const highestTotal = highest.calculateTotal();
+        return currentTotal > highestTotal ? order : highest;
+    }, orders[0]);
+}
